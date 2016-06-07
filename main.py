6ff27ab52b3d3b2f3685f6b6c0f4e18cb0f5f0c5
Lucas Menéndez <hi@lucasmenendez.me>
2016-06-07 22:10:02 +0200

    Set repo name as window title

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Filemanager():
 
     def __init__(self):
         self.instance = view.View()
-        self.instance.master.title("Filemanager")
+        self.instance.master.title("Pylemanager")
         self.instance.mainloop()
 
 
