41c96044b991bac8775e72569ad1df93463ac5b2
Lucas Menéndez <hi@lucasmenendez.me>
2016-06-19 19:19:29 +0200

    Fixed error on delete empty folders

--- controller.py
+++ controller.py
@@ -17,92 +17,88 @@ class Controller():
 
     def copy(self, selection, location):
         if os.path.exists(location):
-            if len(selection) > 0:    
-                for item in selection:
-                    if os.path.exists(item):
-                        if os.path.isdir(item):
-                            dest = location+"/"+self.basename(item)
-                            try:
-                                os.mkdir(dest, 0755)                           
-                            except:
-                                return False
+            for item in selection:
+                if os.path.exists(item):
+                    if os.path.isdir(item):
+                        dest = location+"/"+self.basename(item)
+                        try:
+                            os.mkdir(dest, 0755)                           
+                        except:
+                            return False
 
-                            new_selection = []
-                            for basename in os.listdir(item):
-                                new_selection.append(item+"/"+basename)
+                        new_selection = []
+                        for basename in os.listdir(item):
+                            new_selection.append(item+"/"+basename)
 
-                            if not self.copy(new_selection, dest):
-                                return False
-                        else:
-                            if not self.cp(item, location):
-                                return False
-                return True
+                        if not self.copy(new_selection, dest):
+                            return False
+                    else:
+                        if not self.cp(item, location):
+                            return False
+            return True
         return False
  
     def cut(self, selection, location):
         if os.path.exists(location):
-            if len(selection) > 0:    
-                for item in selection:
-                    if os.path.exists(item):
-                        if os.path.isdir(item):
-                            dest = location+"/"+self.basename(item)
-                            try:
-                                os.mkdir(dest, 0755)                           
-                            except:
-                                return False
-
-                            new_selection = []
-                            for basename in os.listdir(item):
-                                new_selection.append(item+"/"+basename)
-
-                            if self.cut(new_selection, dest):
-                                try:
-                                    os.rmdir(item)
-                                except:
-                                    return False
-                            else:
-                                return False
-                        else:
-                            if self.cp(item, location):
-                                try:
-                                    os.unlink(item)
-                                except:
-                                    return False
-                            else:
-                                return False
-                return True
-        return False
- 
-    def delete(self, selection, location):
-        if len(selection) > 0:
-            for path in selection:
-                if os.path.exists(path):
-                    if os.path.isdir(path):
-                        for src in os.listdir(path):
-                            item = path + "/" + src
-                            if os.path.isdir(item):
-                                new_selection = []
-                                for basename in os.listdir(item):
-                                    new_selection.append(item)
-
-                                if not self.delete(new_selection, item):
-                                    return False
-                            else:
-                                try:
-                                    os.unlink(item)
-                                except:
-                                    return False
+            for item in selection:
+                if os.path.exists(item):
+                    if os.path.isdir(item):
+                        dest = location+"/"+self.basename(item)
                         try:
-                             os.rmdir(path)    
+                            os.mkdir(dest, 0755)                           
                         except:
                             return False
+
+                        new_selection = []
+                        for basename in os.listdir(item):
+                            new_selection.append(item+"/"+basename)
+
+                        if self.cut(new_selection, dest):
+                            try:
+                                os.rmdir(item)
+                            except:
+                                return False
+                        else:
+                            return False
                     else:
-                        try:
-                            os.unlink(path)
-                        except:
+                        if self.cp(item, location):
+                            try:
+                                os.unlink(item)
+                            except:
+                                return False
+                        else:
                             return False
             return True
         return False
+ 
+    def delete(self, selection, location):
+        for path in selection:
+            if os.path.exists(path):
+                if os.path.isdir(path):
+                    for src in os.listdir(path):
+                        item = path + "/" + src
+                        if os.path.isdir(item):
+                            new_selection = []
+                            for basename in os.listdir(item):
+                                new_selection.append(item)
+
+                            if not self.delete(new_selection, item):
+                                return False
+                        else:
+                            try:
+                                os.unlink(item)
+                            except:
+                                return False
+                    try:
+                         os.rmdir(path)    
+                    except:
+                        return False
+                else:
+                    try:
+                        os.unlink(path)
+                    except:
+                        return False
+        return True
 
     def createFolder(self, folder):
         if not os.path.exists(folder):
